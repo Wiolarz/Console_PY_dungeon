09c18c11d579e7f59b13b07d7b2af00e144be27b
Wiolarz <36847708+Wiolarz@users.noreply.github.com>
2021-11-29 22:58:43 +0100

    v0.1

---
+++ source_code/technical/tester.py
@@ -0,0 +1,17 @@
+import unittest
+
+import source_code
+
+class unit_creator(unittest.TestCase):
+
+    def hero_creator(self):
+        test_hero = source_code.units.Hero()
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    unit_creator.hero_creator()
